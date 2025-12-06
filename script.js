785c7fa961bee7f1a77c3e27cdcd00c7f5c70853
WisamHmedi <145149510+WisamHmedi@users.noreply.github.com>
2025-12-06 12:23:29 +0100

    klar

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Checboxen
 const divstylecheckbox = document.getElementById(`divstyle`);
 // Textfältet
-const textfield = document.getElementById(`textfield`);
+const textfield = document.getElementsByClassName(`textfield`);
 // knappen
 const removeBtn = document.getElementById(`removeBtn`);
 // div för resultatet
@@ -9,13 +9,26 @@ const resultBox = document.getElementById(`resultBox`);
 // kontroll utskrifter
 console.log(divstylecheckbox, textfield, removeBtn, resultBox);
 
-function handleinput() {
+function handleInput(e) {
     // hämta värdet från textfältet
     console.log(e.taget);
 
-    const name = e.target.name;
+    const name = e.taget.name;
 
     if (name === "content") {
         resultBox.innerHTML = e.target.value;
     }
  }
+
+ divstylecheckbox.addEventListener("change", function () {
+    const colorValue = document.getElementById("color").value;
+    resultBox.style.backgroundColor = colorValue;
+    });
+
+    for (let filed of textfield) {
+        filed.addEventListener("input", handleinput);
+    }
+
+removeBtn.addEventListener("click", function () {
+    resultBox.remove();
+});
